5b662fc01f51c1ad11461bf83939e9cc0a0af1f3
haiderzia105 <mto.itsc.105@tuf.edu.pk>
2024-08-22 16:59:20 +0500

    Velzon Theme implementation (Working)

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>@yield('title')</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="{{asset('admin/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('admin/css/icons.min.css')}}">
+<link rel="stylesheet" href="{{asset('admin/css/app.min.css')}}">
+<link rel="stylesheet" href="{{asset('admin/css/custom.min.css')}}">
+
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,1 @@
+<script src="{{asset('admin/js/layout.js')}}"></script>
